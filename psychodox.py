676f0c72776d6fbf75a8db518a04ffd2241dd5b6
p5ch05qu4d <128115991+p5ch05qu4d@users.noreply.github.com>
2023-03-16 23:04:56 -0500

    Create psychodox.py

---
+++ psychodox.py
@@ -0,0 +1,195 @@
+from bs4 import BeautifulSoup
+import cfscrape
+from collections import deque
+import json
+import nmap
+import os
+from os import system
+import re
+import requests
+import requests.exceptions
+import requests as res
+from requests import get
+import sys
+import time
+from time import gmtime, strftime
+from urllib.error import URLError
+from urllib.parse import urlsplit
+import urllib3
+import urllib.request
+from urllib.request import urlopen
+import urllib.parse
+import webtech
+
+os.system("reset")
+
+
+def banner():
+    print(""" 
+    
+    
+       .ed"""" """$$$$be.
+     -"           ^""**$$$e.
+   ."                   '$$$c
+  /                      "4$$b
+ d  3                      $$$$
+ $  *                   .$$$$$$
+.$  ^c           $$$$$e$$$$$$$$.
+d$L  4.         4$$$$$$$$$$$$$$b
+$$$$b ^ceeeee.  4$$ECL.F*$$$$$$$
+$$$$P d$$$$F $ $$$$$$$$$- $$$$$$
+3$$$F "$$$$b   $"$$$$$$$  $$$$*"
+ $$P"  "$$b   .$ $$$$$...e$$
+  *c    ..    $$ 3$$$$$$$$$$eF
+    %ce""    $$$  $$$$$$$$$$*
+     *$e.    *** d$$$$$"L$$
+      $$$      4J$$$$$% $$$
+     $"'$=e....$*$$**$cz$$"
+     $  *=%4.$ L L$ P3$$$F
+     $   "%*ebJLzb$e$$$$$b
+      %..      4$$$$$$$$$$
+       $$$e   z$$$$$$$$$$
+        "*$c  "$$$$$$$P"
+          """*$$$$$$$"
+
+Secret...pick 3 ;)""")
+
+time.sleep(2)
+def menu():
+    print("--------------")
+    print("1. whois lookup.")
+    print("2. ip tracking.")
+    print("3. Socials")
+    print("4. exit.")
+    print("--------------")
+def fun():
+    choice = ("1")
+    banner()
+
+    while choice != ("12"):
+        menu()
+        choice = input("enter your choice--> ")
+
+        if choice == ("1"):
+            try:
+                target = input("\033[1;91m[+] Enter Domain or IP Address: \033[1;m").lower()
+                os.system("reset")
+                print("\033[34m[~] Searching for Whois Lookup: \033[0m".format(target) + target)
+                time.sleep(1.5)
+                command = ("whois " + target)
+                proces = os.popen(command)
+                results = str(proces.read())
+                print(results + command)
+
+            except Exception:
+                pass
+
+        elif choice == ("1"):
+            try:
+                os.system("reset")
+                os.system("gnome-terminal -e 'bash -c \"sudo etherape; exec bash\"'")
+
+            except Exception:
+                pass
+
+
+        elif choice == ("2"):
+            try:
+                target = input("Enter Domain or IP Address: ").lower()
+                url = ("http://ip-api.com/json/")
+                response = urllib.request.urlopen(url + target)
+                data = response.read()
+                jso = json.loads(data)
+                os.system("reset")
+                print("Searching IP Location Finder: ".format(url) + target)
+                time.sleep(1.5)
+
+                print("\n Url: " + target + "\033[0m")
+                print("\033[34m" + "IP: " + jso["query"] + "\033[0m")
+                print("\033[34m" + "Status: " + jso["status"] + "\033[0m")
+                print("\033[34m" + "Region: " + jso["regionName"] + "\033[0m")
+                print("\033[34m" + "Country: " + jso["country"] + "\033[0m")
+                print("\033[34m" + "City: " + jso["city"] + "\033[0m")
+                print("\033[34m" + "ISP: " + jso["isp"] + "\033[0m")
+                print("\033[34m" + "Lat & Lon: " + str(jso['lat']) + " & " + str(jso['lon']) + "\033[0m")
+                print("\033[34m" + "Zipcode: " + jso["zip"] + "\033[0m")
+                print("\033[34m" + "TimeZone: " + jso["timezone"] + "\033[0m")
+                print("\033[34m" + "AS: " + jso["as"] + "\033[0m" + "\n")
+
+
+            except URLError:
+                print("enter real ip address.")
+
+        elif choice == ("3"):
+            print("""
+            
+            
+       .ed"""" """$$$$be.
+     -"           ^""**$$$e.
+   ."                   '$$$c
+  /                      "4$$b
+ d  3                      $$$$
+ $  *                   .$$$$$$
+.$  ^c           $$$$$e$$$$$$$$.
+d$L  4.         4$$$$$$$$$$$$$$b
+$$$$b ^ceeeee.  4$$ECL.F*$$$$$$$
+$$$$P d$$$$F $ $$$$$$$$$- $$$$$$
+3$$$F "$$$$b   $"$$$$$$$  $$$$*"
+ $$P"  "$$b   .$ $$$$$...e$$
+  *c    ..    $$ 3$$$$$$$$$$eF
+    %ce""    $$$  $$$$$$$$$$*
+     *$e.    *** d$$$$$"L$$
+      $$$      4J$$$$$% $$$
+     $"'$=e....$*$$**$cz$$"
+     $  *=%4.$ L L$ P3$$$F
+     $   "%*ebJLzb$e$$$$$b
+      %..      4$$$$$$$$$$
+       $$$e   z$$$$$$$$$$
+        "*$c  "$$$$$$$P"
+          """*$$$$$$$"
+     -------------------------
+      !FOLLOW VOIDS SHIT NOW!
+     -------------------------
+            
+            Hello, please take your time to read this message.
+————————————————
+Psychosquad is a team, while small we are still working to get out there. We work on projects, websites, we will even make you a website.
+
+At the moment we have a website, kik group, discord server, instagram page, youtube channel, etc! We hope to be on more! 
+
+On our discord server, we have a how to channel.. which we also do how to on youtube, such as how to access the darkweb which is the first video made.
+
+Our website will contain mods, hacking scripts, sales and more. You can check it out for yourself. 
+
+Heres a list for you to check out below!
+————————————————
+Discord: https://discord.gg/bBc6HTAUtX
+
+Our website: https://psychedelictech.000webhostapp.com/
+
+Kik group: #animesucksdong
+
+Youtube: https://youtube.com/@unw37c0m3d
+
+Instagram: https://instagram.com/psychodelictech?igshid=YmMyMTA2M2Y=
+————————————————
+            """)
+            sys.exit()
+
+
+
+
+        elif choice == ("4"):
+            print("exiting script.")
+            sys.exit()
+
+        else:
+            os.system("reset")
+            print("incorrect option.")
+
+
+# =====# Main #===== #
+
+if __name__ == "__main__":
+    fun()
+
